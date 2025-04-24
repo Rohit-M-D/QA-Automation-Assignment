af69222eea50ba3849a3fea595aeadbfba1249b8
Rohit-M-D <rohit@datman.je>
2025-04-24 13:33:05 +0530

    Changed code in TC03

--- src/test/java/Tests/TC03_GetOtpTest.java
+++ src/test/java/Tests/TC03_GetOtpTest.java
@@ -6,13 +6,13 @@ import Pages.LoginFlow;
 //This test case is for verifying OTP button
 
 public class TC03_GetOtpTest extends LoginFlow {
-    String dummyMobNo = "0000000000";
+    
 
     @Test
     public void otpPageTest() {
         
             clickFirstLoginButton();
-            enterMobileNumber(dummyMobNo);
+            enterMobileNumber(mobNo);
             clickGetOtpBtn();
             
     }
--- src/test/java/Tests/TC04_WrongMobileNumberTest.java
+++ src/test/java/Tests/TC04_WrongMobileNumberTest.java
@@ -8,11 +8,13 @@ import Pages.LoginFlow;
 //This test is for wrong mibile number
 
 public class TC04_WrongMobileNumberTest extends LoginFlow {
+    String dummyMobNo = "0000000000";
     @Test
     public void otpPageTest() {
         
+        
             clickFirstLoginButton();
-            enteredWrongMobileNumber();
+            enterMobileNumber(dummyMobNo);
             clickGetOtpBtn();   
     }
     
